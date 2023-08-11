189d9808a2d8d1a38ae4b45a245b8b6a8038061b
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2023-08-11 18:44:06 +0100

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Elvis on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
